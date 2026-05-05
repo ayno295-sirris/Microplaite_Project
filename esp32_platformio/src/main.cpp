4fa258ac618209d123a71436e23d02effe55e309
Noam Ayaou <noam.ayaou@sirris.be>
2026-05-05 16:04:35 +0200

    Configure ESP32-S3 N32R16V PlatformIO baseline

--- esp32_platformio/src/main.cpp
+++ esp32_platformio/src/main.cpp
@@ -2,10 +2,11 @@
 
 void setup() {
   Serial.begin(115200);
-  delay(200);
+  delay(1000);
   Serial.println("microplaite esp32-s3 boot");
 }
 
 void loop() {
+  Serial.println("heartbeat");
   delay(1000);
 }
